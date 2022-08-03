5b504827bf8c8717da807ba1201800a4dbb39ebe
Hans Huebner <hans.huebner@gmail.com>
2022-08-03 21:49:03 +0200

    First cut at implementing CPLD logic

--- cpld/cpc-fdc-cpld.vhd
+++ cpld/cpc-fdc-cpld.vhd
@@ -26,10 +26,9 @@ entity CPC_FDC is
          LED3: out std_logic;
          LED4: out std_logic;
 
-         READY: out std_logic;
+         READY: in std_logic;
          FLT_TRK0: out std_logic;
-         US0: in std_logic;
-         US1: in std_logic;
+         US: in std_logic_vector(1 downto 0);
          nRW_SEEK: in std_logic;
          nRSET: in std_logic;
          FLTR_STEP: in std_logic;
@@ -39,6 +38,7 @@ entity CPC_FDC is
          nFDCCS: out std_logic;
          WE: in std_logic;
 
+         CLK4: in std_logic;
          A: in std_logic_vector(15 downto 0);
          D0: in std_logic;
          nIORQ: in std_logic;
@@ -48,8 +48,45 @@ entity CPC_FDC is
 end CPC_FDC;
 
 architecture Behavioral of CPC_FDC is
-
+  signal motor: std_logic := '0';
 begin
+
+  process (CLK4, nRSET)
+  begin
+    if falling_edge(CLK4) then
+      if nIORQ = '0' and nWR = '0' and A = x"FA7E" then
+        motor <= D0;
+      elsif nRSET = '0' then
+        motor <= '0';
+      end if;
+    end if;
+  end process;
+
+  MTRON <= motor;
+
+  process (CLK4)
+  begin
+    if falling_edge(CLK4) then
+      if nIORQ = '0' and A(15 downto 1) & '0' = x"FBF6" then
+        nFDCCS <= '0';
+      else
+        nFDCCS <= '1';
+      end if;
+    end if;
+  end process;
+
+  nIORD <= '0' when nIORQ = '0' and nRD = '0' else '1';
+  nIOWR <= '0' when nIORQ = '0' and nWR = '0' else '1';
+  RESET <= not nRSET;
+  FLT_TRK0 <= '1' when TRK00 = '1' and nRW_SEEK = '1' else '0';
+  STEP <= '1' when FLTR_STEP = '1' and nRW_SEEK = '1' else '0';
+  WGATE <= '1' when READY = '1' and WE = '1' else '0';
+
+  nDS0 <= '0' when US = "00" else '1';
+  nDS1 <= '0' when US = "01" else '1';
+  nDS2 <= '0' when US = "10" else '1';
+  nDS3 <= '0' when US = "11" else '1';
+  
 end Behavioral;
 
 
