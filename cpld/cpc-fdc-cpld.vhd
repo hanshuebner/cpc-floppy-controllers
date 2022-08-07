f895aa84ed8702111371f5502f3d58437bf1f170
Hans Huebner <hans.huebner@wagestream.com>
2022-08-07 08:07:25 +0200

    Layout with ROM redone

--- cpld/cpc-fdc-cpld.vhd
+++ cpld/cpc-fdc-cpld.vhd
@@ -7,54 +7,62 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity CPC_FDC is
-  port ( nDS0: out std_logic;
-         nDS1: out std_logic;
-         nDS2: out std_logic;
-         nDS3: out std_logic;
+  port ( nRW_SEEK: in std_logic;
+         FLTR_STEP: in std_logic;
+         FLT_TRK0: out std_logic;
+         US: in std_logic_vector(1 downto 0);
+         WE: in std_logic;
+         RESET: out std_logic;
+         nIORD: out std_logic;
+         nIOWR: out std_logic;
+         nFDCCS: out std_logic;
+
+         LED1: out std_logic;
+         nVORTEX_CPC: in std_logic;
+
          STEP: out std_logic;
          WGATE: out std_logic;
          TRK00: in std_logic;
          MTRON: out std_logic;
+         nDS0: out std_logic;
+         nDS1: out std_logic;
+         nDS2: out std_logic;
+         nDS3: out std_logic;
 
-         CFG1: in std_logic;
-         CFG2: in std_logic;
-         CFG3: in std_logic;
-         CFG4: in std_logic;
-
-         LED1: out std_logic;
-         LED2: out std_logic;
-         LED3: out std_logic;
-         LED4: out std_logic;
-
-         READY: in std_logic;
-         FLT_TRK0: out std_logic;
-         US: in std_logic_vector(1 downto 0);
-         nRW_SEEK: in std_logic;
-         nRSET: in std_logic;
-         FLTR_STEP: in std_logic;
-         nIORD: out std_logic;
-         nIOWR: out std_logic;
-         RESET: out std_logic;
-         nFDCCS: out std_logic;
-         WE: in std_logic;
+         ROMA14: out std_logic;
+         ROMA15: out std_logic;
+         ROMA16: out std_logic;
+         nROMWE: out std_logic;
+         nROMOE: out std_logic;
+         nROMCE: out std_logic;
 
          CLK4: in std_logic;
-         A: in std_logic_vector(15 downto 0);
-         D0: in std_logic;
-         nIORQ: in std_logic;
+         nRSET: in std_logic;
+         nROMEN: in std_logic;
+         ROMDIS: out std_logic;
+         READY: in std_logic;
          nWR: in std_logic;
-         nRD: in std_logic
+         nRD: in std_logic;
+         nIORQ: in std_logic;
+         nMREQ: in std_logic;
+
+         D0: in std_logic;
+
+         A13: in std_logic;
+         A: in std_logic_vector(10 downto 0);
 );
 end CPC_FDC;
 
 architecture Behavioral of CPC_FDC is
   signal motor: std_logic := '0';
+  signal fdc_address: std_logic_vector(10 downto 0) := A(10 downto 1) & '0';
+  signal fdc_select_address: std_logic_vector(10 downto 0) := x"3F6" when nVORTEX_CPC = '0' else x"37E;
 begin
 
   process (CLK4, nRSET)
   begin
     if falling_edge(CLK4) then
-      if nIORQ = '0' and nWR = '0' and A = x"FA7E" then
+      if nIORQ = '0' and nWR = '0' and A13 = '0' then
         motor <= D0;
       elsif nRSET = '0' then
         motor <= '0';
@@ -67,7 +75,7 @@ begin
   process (CLK4)
   begin
     if rising_edge(CLK4) then
-      if nIORQ = '0' and A(15 downto 1) & '0' = x"FBF6" then
+      if nIORQ = '0' and fdc_address = fdc_select_address then
         nFDCCS <= '0';
       else
         nFDCCS <= '1';
