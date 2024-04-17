6f6a63378c98d7c6b3d423c449f5eb4795dcabb3
Hans Hübner <hans.huebner@gmail.com>
2024-04-17 11:14:39 +0200

    first attempt with cpld

--- cpld/cpc-fdc-cpld.vhd
+++ cpld/cpc-fdc-cpld.vhd
@@ -5,14 +5,14 @@ use ieee.numeric_std.all;
 entity cpc_fdc is
   port (
     -- CPC bus inputs
-    address        : in std_logic_vector(15 downto 0);
+    a              : in std_logic_vector(15 downto 0);
     d0             : in std_logic;
     rd_n           : in std_logic;
     wr_n           : in std_logic;
-    ioreq_n        : in std_logic;
+    iorq_n         : in std_logic;
     clk4           : in std_logic;
     rset_n         : in std_logic;
-    
+
     -- FDC control signal outputs
     ldcr_n         : out std_logic;
     ldor_n         : out std_logic;
@@ -31,17 +31,18 @@ end cpc_fdc;
 
 architecture rtl of cpc_fdc is
   signal motor_latch        : std_logic := '0';
+  constant FD_MOTOR_ADDR    : std_logic_vector(15 downto 0) := x"FA7E";
   constant VORTEX_FDC_ADDR  : std_logic_vector(15 downto 0) := x"FBF6";
   constant AMSTRAD_FDC_ADDR : std_logic_vector(15 downto 0) := x"FB7E";
 begin
 
   -- Motor Latching
-  process (clk4, wr_n, address, d0, rset_n)
+  process (clk4, wr_n, a, d0, rset_n)
   begin
     if rising_edge(clk4) then
       if rset_n = '0' then
         motor_latch <= '0';
-      elsif wr_n = '0' and ioreq_n = '0' and address = x"FA7E" then
+      elsif wr_n = '0' and iorq_n = '0' and a = FD_MOTOR_ADDR then
         motor_latch <= not d0;
       end if;
     end if;
@@ -51,11 +52,11 @@ begin
   mtron_n <= motor_latch;
 
   -- FDC control signals
-  fdccs_n <= '0' when ((address_select = '1' and address(15 downto 1) = VORTEX_FDC_ADDR(15 downto 1))
-                       or (address_select = '0' and address(15 downto 1) = AMSTRAD_FDC_ADDR(15 downto 1)))
+  fdccs_n <= '0' when ((address_select = '1' and a(15 downto 1) = VORTEX_FDC_ADDR(15 downto 1))
+                       or (address_select = '0' and a(15 downto 1) = AMSTRAD_FDC_ADDR(15 downto 1)))
              else '1';
-  iowr_n  <= wr_n or ioreq_n;
-  iord_n  <= rd_n or ioreq_n;
+  iowr_n  <= wr_n or iorq_n;
+  iord_n  <= rd_n or iorq_n;
   reset   <= not rset_n;
   ldcr_n  <= '1';
   ldor_n  <= '1';
